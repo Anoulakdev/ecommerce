d54ddd72a855e25c85997230bbb82019cc3c8e8b
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-08-15 11:39:32 +0700

    556156415

--- routers/user.js
+++ routers/user.js
@@ -26,7 +26,7 @@ router.get("/users/admin", auth, checkRole([2]), listadmin);
 router.get("/users/user", auth, checkRole([3]), listuser);
 router.get("/users/:userId", auth, getById);
 router.get("/users/profileview/:code", auth, profileview);
-router.post("/users", auth, create);
+router.post("/users", create);
 router.post("/users/changepassword", auth, changepassword);
 router.put("/users/:userId", auth, update);
 router.put("/users/updateprofile/:userId", auth, updateprofile);
